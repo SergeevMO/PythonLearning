8ec91578573ada0bc2ba8223650d5d039e7eb503
Mihail Sergeev <Mihail-Ol-Sergeev@yande.ru>
2021-09-13 16:12:42 +0300

    Добавлены ещё два варианта решения задачи 221

--- Хилл К. Научное программирование на Python (2, 2021)/Глава 2/Упражнения 2.2.6/Задачи/E221.py
+++ Хилл К. Научное программирование на Python (2, 2021)/Глава 2/Упражнения 2.2.6/Задачи/E221.py
@@ -1,4 +1,4 @@
-# Способ № 1
+print('Способ № 1')
 # Не-И И ИЛИ
 a = True
 b = True
@@ -21,7 +21,8 @@ x = not(a and b) and (a or b)
 print(x)
 
 
-# Способ № 2
+
+print('Способ № 2')
 # И ИЛИ НЕ-ИЛИ
 a = True
 b = True
@@ -41,4 +42,52 @@ print(x)
 a = False
 b = False
 x = (a and b) or not(a or b)
+print(x)
+
+
+
+print('Способ № 3')
+# (НЕ И) ИЛИ (НЕ И)
+a = True
+b = True
+x = (not a and b) or (not b and a)
+print(x)
+
+a = False
+b = True
+x = (not a and b) or (not b and a)
+print(x)
+
+a = True
+b = False
+x = (not a and b) or (not b and a)
+print(x)
+
+a = False
+b = False
+x = (not a and b) or (not b and a)
+print(x)
+
+
+
+print('Способ № 4')
+# НЕ РАВНО
+a = True
+b = True
+x = a != b
+print(x)
+
+a = False
+b = True
+x = a != b
+print(x)
+
+a = True
+b = False
+x = a != b
+print(x)
+
+a = False
+b = False
+x = a != b
 print(x)
