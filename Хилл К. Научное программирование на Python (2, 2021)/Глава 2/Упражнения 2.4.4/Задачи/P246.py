54d36d4e827ea999efcccbffe2f0c3dead3f883a
Mihail Sergeev <Mihail-Ol-Sergeev@yandex.ru>
2021-12-24 16:28:01 +0300

    Дополнен файл P2.4.6

--- Хилл К. Научное программирование на Python (2, 2021)/Глава 2/Упражнения 2.4.4/Задачи/P246.py
+++ Хилл К. Научное программирование на Python (2, 2021)/Глава 2/Упражнения 2.4.4/Задачи/P246.py
@@ -1,5 +1,5 @@
 # Двойной факториал
-n = 10
+n = 11
 f = 1
 
 if n % 2 == 1:
@@ -16,7 +16,15 @@ print(f)
 
 print('\n')
 
+# Calcualate n!! = 1.3.5.7...(n-2)n for odd n
+n = 11
+dfactorial = 1
+for i in range(3, n+1, 2):
+    dfactorial *= i
+print('{:d}!! = {:d}'.format(n, dfactorial))
+
 # Вариант для чётного двойного факториала
+# Calcualate n!! = 2.4.6.8...(n-2)n for even n
 import math
 n = 10
 dfactorial = 2**(n // 2) * math.factorial(n // 2)
